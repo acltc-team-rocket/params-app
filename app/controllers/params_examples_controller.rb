8ad1948cfd0aef01301b54f1ef06903b76d965c2
Mark Richardson <mark@markeban.com>
2016-07-24 10:49:30 -0700

    finished query examples

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -5,4 +5,30 @@ class ParamsExamplesController < ApplicationController
     @message = params[:message]
     @stuff = params[:stuff]
   end
+
+  def name
+    @name = params[:name].upcase
+    if @name.start_with?("A")
+      @message = "Hey!!! Your name starts with 'A'"
+    else
+      @message = "You had to be in the back of the line in grade school."
+    end
+  end
+
+  def guess_the_number
+    @guess = params[:guess].to_i
+    correct_number = 36
+    if @guess < correct_number
+      @message = "guess higher!!"
+    elsif @guess > correct_number
+      @message = "guess lower!!!"
+    else
+      @message = "you win!!!!"
+    end
+  end
+
+  def counter
+    @view_number = params[:number].to_i + 1
+  end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get '/query_parameters', to: 'params_examples#query_parameters'
+  get '/name', to: 'params_examples#name'
+  get '/guess_the_number', to: 'params_examples#guess_the_number'
+  get '/counter', to: 'params_examples#counter'
 end
